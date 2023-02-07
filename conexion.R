ab436ff775f73f58baa9f3c9768d9f5c2633f7bb
Asun M. Mayoral <asun.mayoral@umh.es>
2023-02-07 13:50:24 +0100

    primer commit

---
+++ conexion.R
@@ -0,0 +1,9 @@
+# Conexión de GitHub con RStudio
+# Crear un nuevo repositorio en nuestra cuenta de Github
+# Copiar al portapapeles la URL del repositorio
+repo= https://github.com/asunmayoral/github_r.git
+# En RStudio crear “New project>Version Control>Git”.
+# Introducir en el cuadro de texto la url del repositorio
+# Pulsamos “Create Project”.
+# A continuación podrá consultarse la pestaña “Git” 
+# y ver la información asociada al repositorio descargado.
